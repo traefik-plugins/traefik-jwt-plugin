2c45ff4d25243469060568923a9f97b7e67a6e01
Barry Lagerweij <b.lagerweij@carepay.co.ke>
2021-05-28 23:06:43 +0200

    Always inject JWT headers, even when we do not validate the signature

--- jwt.go
+++ jwt.go
@@ -195,98 +195,98 @@ func (jwtPlugin *JwtPlugin) ParseKeys(certificates []string) error {
 }
 
 func (jwtPlugin *JwtPlugin) FetchKeys() {
-		for _, u := range jwtPlugin.jwkEndpoints {
-			response, err := http.Get(u.String())
-			if err != nil {
-				// TODO: log warning
-				continue
-			}
-			body, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				// TODO: log warning
-				continue
-			}
-			var jwksKeys Keys
-			err = json.Unmarshal(body, &jwksKeys)
-			if err != nil {
-				// TODO: log warning
-				continue
-			}
-			for _, key := range jwksKeys.Keys {
-				switch key.Kty {
-				case "RSA":
-					{
-						if key.Kid == "" {
-							key.Kid, err = JWKThumbprint(fmt.Sprintf(`{"e":"%s","kty":"RSA","n":"%s"}`, key.E, key.N))
-							if err != nil {
-								break
-							}
-						}
-						nBytes, err := base64.RawURLEncoding.DecodeString(key.N)
+	for _, u := range jwtPlugin.jwkEndpoints {
+		response, err := http.Get(u.String())
+		if err != nil {
+			// TODO: log warning
+			continue
+		}
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			// TODO: log warning
+			continue
+		}
+		var jwksKeys Keys
+		err = json.Unmarshal(body, &jwksKeys)
+		if err != nil {
+			// TODO: log warning
+			continue
+		}
+		for _, key := range jwksKeys.Keys {
+			switch key.Kty {
+			case "RSA":
+				{
+					if key.Kid == "" {
+						key.Kid, err = JWKThumbprint(fmt.Sprintf(`{"e":"%s","kty":"RSA","n":"%s"}`, key.E, key.N))
 						if err != nil {
 							break
 						}
-						eBytes, err := base64.RawURLEncoding.DecodeString(key.E)
-						if err != nil {
-							break
-						}
-						jwtPlugin.keys[key.Kid] = &rsa.PublicKey{N: new(big.Int).SetBytes(nBytes), E: int(new(big.Int).SetBytes(eBytes).Uint64())}
 					}
-				case "EC":
-					{
-						if key.Kid == "" {
-							key.Kid, err = JWKThumbprint(fmt.Sprintf(`{"crv":"P-256","kty":"EC","x":"%s","y":"%s"}`, key.X, key.Y))
-							if err != nil {
-								break
-							}
+					nBytes, err := base64.RawURLEncoding.DecodeString(key.N)
+					if err != nil {
+						break
+					}
+					eBytes, err := base64.RawURLEncoding.DecodeString(key.E)
+					if err != nil {
+						break
+					}
+					jwtPlugin.keys[key.Kid] = &rsa.PublicKey{N: new(big.Int).SetBytes(nBytes), E: int(new(big.Int).SetBytes(eBytes).Uint64())}
+				}
+			case "EC":
+				{
+					if key.Kid == "" {
+						key.Kid, err = JWKThumbprint(fmt.Sprintf(`{"crv":"P-256","kty":"EC","x":"%s","y":"%s"}`, key.X, key.Y))
+						if err != nil {
+							break
 						}
-						var crv elliptic.Curve
-						switch key.Crv {
-						case "P-256":
+					}
+					var crv elliptic.Curve
+					switch key.Crv {
+					case "P-256":
+						crv = elliptic.P256()
+					case "P-384":
+						crv = elliptic.P384()
+					case "P-521":
+						crv = elliptic.P521()
+					default:
+						switch key.Alg {
+						case "ES256":
 							crv = elliptic.P256()
-						case "P-384":
+						case "ES384":
 							crv = elliptic.P384()
-						case "P-521":
+						case "ES512":
 							crv = elliptic.P521()
 						default:
-							switch key.Alg {
-							case "ES256":
-								crv = elliptic.P256()
-							case "ES384":
-								crv = elliptic.P384()
-							case "ES512":
-								crv = elliptic.P521()
-							default:
-								crv = elliptic.P256()
-							}
+							crv = elliptic.P256()
 						}
-						xBytes, err := base64.RawURLEncoding.DecodeString(key.X)
-						if err != nil {
-							break
-						}
-						yBytes, err := base64.RawURLEncoding.DecodeString(key.Y)
-						if err != nil {
-							break
-						}
-						jwtPlugin.keys[key.Kid] = &ecdsa.PublicKey{Curve: crv, X: new(big.Int).SetBytes(xBytes), Y: new(big.Int).SetBytes(yBytes)}
 					}
-				case "oct":
-					{
-						kBytes, err := base64.RawURLEncoding.DecodeString(key.K)
+					xBytes, err := base64.RawURLEncoding.DecodeString(key.X)
+					if err != nil {
+						break
+					}
+					yBytes, err := base64.RawURLEncoding.DecodeString(key.Y)
+					if err != nil {
+						break
+					}
+					jwtPlugin.keys[key.Kid] = &ecdsa.PublicKey{Curve: crv, X: new(big.Int).SetBytes(xBytes), Y: new(big.Int).SetBytes(yBytes)}
+				}
+			case "oct":
+				{
+					kBytes, err := base64.RawURLEncoding.DecodeString(key.K)
+					if err != nil {
+						break
+					}
+					if key.Kid == "" {
+						key.Kid, err = JWKThumbprint(key.K)
 						if err != nil {
 							break
 						}
-						if key.Kid == "" {
-							key.Kid, err = JWKThumbprint(key.K)
-							if err != nil {
-								break
-							}
-						}
-						jwtPlugin.keys[key.Kid] = kBytes
 					}
+					jwtPlugin.keys[key.Kid] = kBytes
 				}
 			}
 		}
+	}
 }
 
 func (jwtPlugin *JwtPlugin) ServeHTTP(rw http.ResponseWriter, request *http.Request) {
@@ -308,12 +308,6 @@ func (jwtPlugin *JwtPlugin) CheckToken(request *http.Request) error {
 			if err = jwtPlugin.VerifyToken(jwtToken); err != nil {
 				return err
 			}
-			for k, v := range jwtPlugin.jwtHeaders {
-				value, ok := jwtToken.Payload[v]
-				if ok {
-					request.Header.Add(k, value.(string))
-				}
-			}
 		}
 		for _, fieldName := range jwtPlugin.payloadFields {
 			if _, ok := jwtToken.Payload[fieldName]; !ok {
@@ -331,6 +325,12 @@ func (jwtPlugin *JwtPlugin) CheckToken(request *http.Request) error {
 					})
 					fmt.Println(string(jsonLogEvent))
 				}
+			}
+		}
+		for k, v := range jwtPlugin.jwtHeaders {
+			value, ok := jwtToken.Payload[v]
+			if ok {
+				request.Header.Add(k, value.(string))
 			}
 		}
 	}
